5e6f1d85caef36cf23f04ee39d5ce34832cf377c
Saurabh Shinde <shindesaurabhpict@gmail.com>
2025-08-24 05:05:06 +0000

    Hardening shell script and build script

--- utils.sh
+++ utils.sh
@@ -1,8 +1,17 @@
-rotate() {
+#!/bin/bash
+set -e
+
+function rotate() {
   fname="$1"
   if [ -z "$fname" ]; then
     echo "Usage: $0 <fname>"
     exit 1
   fi
   mv "$1" "$1.orig"
+}
+
+function build_img() {
+  [[ ! -f Dockerfile ]] && exit 1
+
+  docker build -t ebpf-playground:0.1 .
 }
